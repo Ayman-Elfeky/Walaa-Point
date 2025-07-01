351ffdb00386239e37ea63ae8f1edd992e8853e3
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-07-01 18:42:51 +0300

    Add sendEmail method

---
+++ utils/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+module.exports.sendEmail = async (userEmail, subject, htmlTemplate) => {
+    console.log("Receive user email successfully: ", userEmail);
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.APP_EMAIL_ADDRESS,
+                pass: process.env.APP_EMAIL_PASSWORD
+            }
+        });
+
+        const mailOptions = {
+            from: `"Loyalfy" <${process.env.APP_EMAIL_ADDRESS}>`,
+            to: 'aywork73@gmail.com',
+            subject: subject,
+            html: htmlTemplate
+        }
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent: ", info.response);
+    } catch (error) {
+        console.log("Error: ", error.message);
+        throw new Error("Internal Server Error: nodemailer\n", error.message)
+    }
+}
